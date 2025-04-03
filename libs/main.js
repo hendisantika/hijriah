ba6cc895e4d6d7598481944dc0970420194c5571
Sami Kalammallah <skalexsong@gmail.com>
2025-04-03 08:08:49 +0700

    fix error

--- libs/main.js
+++ libs/main.js
@@ -1,17 +1,18 @@
 // css
 import 'admin-lte/dist/css/adminlte.min.css';
-import 'admin-lte/plugins/fontawesome-free/css/all.min.css';
-import 'admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css';
-import 'admin-lte/plugins/select2/css/select2.min.css';
-import 'admin-lte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css';
-import 'admin-lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css';
+// import 'admin-lte/plugins/fontawesome-free/css/all.min.css';
+// import 'admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css';
+// import 'admin-lte/plugins/select2/css/select2.min.css';
+// import 'admin-lte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css';
+// import 'admin-lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css';
+import 'overlayscrollbars/styles/overlayscrollbars.css';
 
 
 // script
 import 'expose-loader?$!expose-loader?jQuery!jquery';
 import 'bootstrap';
 import 'admin-lte';
-import 'admin-lte/plugins/select2/js/select2.full.min.js';
+// import 'admin-lte/plugins/select2/js/select2.full.min.js';
 
 // momentjs
 import moment from 'moment';
